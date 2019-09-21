349dd25a83d1e5b8b87bdefcdc5ad10352e270c6
admin-at-371541600170 <alex.li@carsales.com.au>
2019-09-21 20:40:20 +1000

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+
+const app = express();
+
+//middleware
+
+function requestCommming(req, res, next) {
+    console.log("request is comming")
+    next();
+}
+
+function authCheck() {
+    // check
+
+}
+
+app.get('/mecca/:product', requestCommming, function(req, res){
+    requestCommming()
+    authCheck()
+    res.send(`mecca ${req.params['product']}`)
+});
+
+app.get('/carsales', function(req, res){
+    requestCommming()
+    authCheck()
+    res.send("carsales")
+});
+
+
+
+
+app.listen(5777, () => {
+    console.log('server is run on 5777')
+});
